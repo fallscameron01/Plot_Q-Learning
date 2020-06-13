12eb6b956e89f58d97f65883ac4145d81024cf3a
Cameron Falls <camfalls0201@gmail.com>
2020-06-13 16:40:19 -0500

    Initial Structure

---
+++ Plot.py
@@ -0,0 +1,35 @@
+"""
+A tool for plotting and visualizing data from Q-Learning projects using the Pandas library.
+"""
+
+import numpy as np
+import pandas as pd
+
+class Plot:
+    """
+    Tool for visualizing Q-Learning.
+
+    Parameters
+    ----------
+    actionSpace: array_like
+    """
+    def __init__(self, actionSpace):
+        self.actions = np.array(actionSpace)
+        self.qTable = {}
+
+    def addState(self, state, actions=None):
+        """
+        Adds or updates a state and its actions to the Q-Table.
+
+        Parameters
+        ----------
+        state: any
+            The state to add.
+        actions: dictionary {any : float}
+            The actions as keys with floats as values. Must be the same actions as in the action space.
+        
+        Returns
+        -------
+        None
+        """
+        self.qTable.update({state : actions})
